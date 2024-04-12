cc21444d3103f9181e922621d263833c9258f053
Foshati <amirrezafoshati@gmail.com>
2024-04-12 22:39:59 +0330

    connect localStorage

--- src/main.ts
+++ src/main.ts
@@ -13,23 +13,35 @@ const form = document.querySelector<HTMLFormElement>("#note-form");
 const title = document.querySelector<HTMLInputElement>("#title");
 const text = document.querySelector<HTMLInputElement>("#text");
 const noteList = document.querySelector<HTMLDivElement>("#itemNote");
+const noteLocalStorage: itemType[] = parsLocalStorage();
+noteLocalStorage.forEach(addNote);
+const clearButton = document.querySelector("#clear-notes");
 
 //? the first method
 //add event listener for select element and print in console
 
 form?.addEventListener("submit", (e) => {
   e.preventDefault();
-  //   error handling
+  // error handling
   if (title?.value == undefined || text?.value == undefined) return;
   const listNote: itemType = {
     id: uuidv4(),
     title: title?.value,
     text: text?.value,
   };
+
+  noteLocalStorage.push(listNote);
+
   addNote(listNote);
+
+  saveLocalStorage();
 
   title.value = "";
   text.value = "";
+});
+
+clearButton?.addEventListener("click", () => {
+  clearLocalStorage();
 });
 
 // create Function Declaration for  render object to window
@@ -55,6 +67,26 @@ function addNote(itemNote: itemType) {
   );
   titleElement.classList.add("text-2xl", "text-blue-800");
 }
+
+function saveLocalStorage() {
+  localStorage.setItem("items", JSON.stringify(noteLocalStorage));
+}
+
+function parsLocalStorage(): itemType[] {
+  const data = localStorage.getItem("items");
+  if (data == null) return [];
+  return JSON.parse(data);
+}
+
+function clearLocalStorage(): void {
+  localStorage.removeItem("items");
+  noteList.innerHTML = "";
+  noteLocalStorage = [];
+}
+
+
+
+
 
 //? the second method
 //!add event listener for select element and print in console
